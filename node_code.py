3867b1b1b83377f678bba684de6dace22af24c62
Dustin <dustinjsanders@protonmail.com>
2021-07-13 00:10:15 +0200

    initial commit

---
+++ node_code.py
@@ -0,0 +1,93 @@
+import RPi.GPIO as GPIO
+import time
+import board
+import busio
+import adafruit_sgp30
+import ctypes
+from influxdb import InfluxDBClient
+
+
+class SHTC3:
+    def __init__(self):
+        self.dll = ctypes.CDLL("./SHTC3.so")  # this needs to be the path to the .so file
+        init = self.dll.init
+        init.restype = ctypes.c_int
+        init.argtypes = [ctypes.c_void_p]
+        init(None)
+
+    def SHTC3_Read_Temperature(self):
+        temperature = self.dll.SHTC3_Read_TH
+        temperature.restype = ctypes.c_float
+        temperature.argtypes = [ctypes.c_void_p]
+        return temperature(None)
+
+    def SHTC3_Read_Humidity(self):
+        humidity = self.dll.SHTC3_Read_RH
+        humidity.restype = ctypes.c_float
+        humidity.argtypes = [ctypes.c_void_p]
+        return humidity(None)
+
+
+shtc3 = SHTC3()
+temperature = round(shtc3.SHTC3_Read_Temperature())
+humidity = round(shtc3.SHTC3_Read_Humidity())
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+
+# Create library object on our I2C port
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+
+sgp30.iaq_init()
+sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+
+eco2 = 0
+
+GPIO.setwarnings(False)
+
+green = 18
+yellow = 27
+red = 22
+
+# numbering scheme
+GPIO.setmode(GPIO.BCM)
+
+# set pins to out
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+
+# set all lights to off
+GPIO.output(red, False)
+GPIO.output(yellow, False)
+GPIO.output(green, False)
+
+while True:
+    eco2 = sgp30.eCO2
+    print(f"eco2 = {eco2}")
+
+    if eco2 <= 1000:
+        GPIO.output(green, True)
+        GPIO.output(yellow, False)
+        GPIO.output(red, False)
+
+    if eco2 >= 1001 and eco2 <= 2000:
+        GPIO.output(green, False)
+        GPIO.output(yellow, True)
+        GPIO.output(red, False)
+
+    if eco2 >= 2001:
+        GPIO.output(green, False)
+        GPIO.output(yellow, False)
+        GPIO.output(red, True)
+
+    time.sleep(1)
+
+    client = InfluxDBClient(host='192.168.178.18',
+                            port='8086',
+                            username='livingroom',
+                            password='livingroom')
+    line = f"environments,room=livingroom temperature={temperature},humidity={humidity},eco2={eco2}"
+    client.write([line], {'db': 'environments'}, 204, 'line')
+    client.close()
+
+
